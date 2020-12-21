ce93e47c84f49e9e589a71bf4b6b015d11165175
Simant-Git <simant.setu@hotmail.com>
2020-12-21 15:15:13 +0530

    added div

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -11,11 +11,14 @@ public class Calc {
 	public static int prod(int num1,int num2){
 		return num1*num2;
 	}
+	public static int div(int num1,int num2){
+		return num1/num2;
+	}
 	public static void main(String[] args) {
 		System.out.println("Sum="+add(10,20));
 		System.out.println("Difference="+subs(90,20));
 		System.out.println("Product="+prod(90,20));
-
+		System.out.println("Quotient="+prod(10,2));
 	}
 
 }
