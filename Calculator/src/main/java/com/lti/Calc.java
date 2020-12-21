2e3458ceb54b622bf5d8ec6e5aef43d918d4a2d4
Simant-Git <simant.setu@hotmail.com>
2020-12-21 15:08:20 +0530

    added prod

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,9 +8,13 @@ public class Calc {
 	public static int subs(int num1,int num2){
 		return num1-num2;
 	}
+	public static int prod(int num1,int num2){
+		return num1*num2;
+	}
 	public static void main(String[] args) {
 		System.out.println("Sum="+add(10,20));
 		System.out.println("Difference="+subs(90,20));
+		System.out.println("Product="+prod(90,20));
 
 	}
 
